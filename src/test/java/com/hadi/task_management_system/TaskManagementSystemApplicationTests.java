0045949973874f976e30a0b1b161d7da398699a5
xhadix <hadixakbar@gmail.com>
2025-04-30 15:12:41 +0700

    init project

---
+++ src/test/java/com/hadi/task_management_system/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadi.task_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
